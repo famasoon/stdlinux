1001ed4cc3a4e6b5c6b506813fda2fa506ebad65
mint <famasoon@gmail.com>
2016-10-09 14:20:22 +0900

    chapter1のソースコード追加

---
+++ chapter1/hello/argc.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main(){
+    fmt.Println("argc =", len(os.Args))
+    for i := 0; i < len(os.Args); i++ {
+        fmt.Printf("argv[%d] = %s\n", i, os.Args[i])
+    }
+    return
+}
