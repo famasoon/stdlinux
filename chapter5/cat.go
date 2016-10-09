0e196196453b334d3c062db774856d30fc8828e7
mint <famasoon@gmail.com>
2016-10-09 17:05:06 +0900

    catコマンドもどき作成

---
+++ chapter5/cat.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+)
+
+func main(){
+    if len(os.Args) < 2 {
+        fmt.Fprintf(os.Stderr, "%s: file name not given\n", os.Args[0])
+        return
+    }
+
+    for i := 1; i < len(os.Args); i++ {
+        do_cat(os.Args[i])
+    }
+}
+
+func do_cat(path string) {
+    fd, err := os.Open(path)
+    if err != nil {
+        panic(err)
+    }
+    defer fd.Close()
+
+    scanner := bufio.NewScanner(fd)
+
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+    }
+    if err := scanner.Err(); err != nil {
+        panic(err)
+    }
+}
